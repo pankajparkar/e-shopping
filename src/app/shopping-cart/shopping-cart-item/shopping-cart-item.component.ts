2562e6a7c6e657f5095f274e553b3e3781178f4e
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-07-27 08:48:49 +0800

    Added onPush strategy on component.

--- src/app/shopping-cart/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/shopping-cart/shopping-cart-item/shopping-cart-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { CartItem } from 'src/app/models/cart-item';
 
 @Component({
   selector: 'shopping-cart-item',
   templateUrl: './shopping-cart-item.component.html',
-  styleUrls: ['./shopping-cart-item.component.css']
+  styleUrls: ['./shopping-cart-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShoppingCartItemComponent implements OnInit {
+
+  @Input() cartItem: CartItem;
 
   constructor() { }
 
